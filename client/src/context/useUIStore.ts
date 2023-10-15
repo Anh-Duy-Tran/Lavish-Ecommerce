3efa7d527f55d9e3fa53557e84ba5045c6af75c3
Duy Tran <trananhduy041003@gmail.com>
2023-10-15 13:47:21 +0300

    fix(client): fix prettier

--- client/src/context/useUIStore.ts
+++ client/src/context/useUIStore.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
 interface UIStoreType {
-  bears: number
-  increase: (by: number) => void
+  bears: number;
+  increase: (by: number) => void;
 }
 
 export const useUIStore = create<UIStoreType>()((set) => ({
   bears: 0,
   increase: (by) => set((state) => ({ bears: state.bears + by })),
-}))
+}));
