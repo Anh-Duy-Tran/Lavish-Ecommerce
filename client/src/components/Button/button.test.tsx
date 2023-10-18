59ec683aa1cf54fdedc41b51daa8d2f8f8858528
Duy Tran <trananhduy041003@gmail.com>
2023-10-18 12:55:22 +0300

    feat(client): change button component to use tailwind variant instead of styled component

--- client/src/components/Button/button.test.tsx
+++ client/src/components/Button/button.test.tsx
@@ -19,44 +19,28 @@ describe("StyledButton Component", () => {
   it("renders correctly", () => {
     const { asFragment } = render(
       <>
-        <Button variant="contained" label="Snapshot1" />
-        <Button variant="outlined" label="Snapshot2" />
-        <Button variant="text" label="Snapshot3" />
+        <Button variant="contained">Snapshot1</Button>
+        <Button variant="outlined">Snapshot1</Button>
+        <Button variant="text">Snapshot1</Button>
       </>,
     );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders label content correctly", () => {
-    render(<Button label="Click me" />);
+    render(<Button>Click me</Button>);
     expect(screen.getByText("Click me"));
-  });
-
-  it("renders children when passed", () => {
-    render(<Button>Children</Button>);
-    expect(screen.getByText("Children")).toBeInTheDocument();
-  });
-
-  it("renders label over children when both are passed", () => {
-    render(<Button label="Label">Children</Button>);
-    expect(screen.getByText("Label")).toBeInTheDocument();
-    expect(screen.queryByText("Children")).not.toBeInTheDocument();
   });
 
   it("handles click event", async () => {
     const handleClick = jest.fn();
-    render(<Button onClick={handleClick} label="Click me" />);
+    render(<Button onClick={handleClick}>Click me</Button>);
     await userEvent.click(screen.getByText("Click me"));
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
-  it("applies the correct width when the width prop is provided", () => {
-    render(<Button width={300} label="Click me" />);
-    expect(screen.getByRole("button")).toHaveStyle("width: 300px");
-  });
-
   it("occupies all available space when fullWidth prop is true", () => {
-    render(<Button fullWidth label="Click me" />);
-    expect(screen.getByRole("button")).toHaveStyle("width: 100%");
+    render(<Button fullWidth>Click me</Button>);
+    expect(screen.getByRole("button")).toHaveClass("w-full");
   });
 });
