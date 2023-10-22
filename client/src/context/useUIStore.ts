79bb15ed19f9f8ffd3994eec2da15d0384e8eb3d
Duy Tran <trananhduy041003@gmail.com>
2023-10-22 04:28:04 +0300

    feat(client): add message modal state and function to UI store

--- client/src/context/useUIStore.ts
+++ client/src/context/useUIStore.ts
@@ -1,18 +1,47 @@
 import { create } from "zustand";
 
+type MessageModalContent = {
+  title: string;
+  message: string;
+  loading?: boolean;
+};
+
 interface UIStoreType {
   currentCategoryIndex: number;
+  setCurrentCategoryIndex: (i: number) => void;
+
   isSidebarOpen: boolean;
   toggleSidebar: () => void;
-  closeSidebar: () => void;
-  setCurrentCategoryIndex: (i: number) => void;
+
+  isMessageModalOpen: boolean;
+  messageModalContent: MessageModalContent;
+  setMessageModalContent: (content: MessageModalContent) => void;
+  setLoadingModalContent: <T>(
+    promise: Promise<T>,
+    resolver: (input: T) => MessageModalContent
+  ) => void;
 }
 
 export const useUIStore = create<UIStoreType>()((set) => ({
   currentCategoryIndex: 0,
+  setCurrentCategoryIndex: (i) => set(() => ({ currentCategoryIndex: i })),
+
   isSidebarOpen: false,
-  closeSidebar: () => set(() => ({ isSidebarOpen: false })),
   toggleSidebar: () =>
     set(({ isSidebarOpen }) => ({ isSidebarOpen: !isSidebarOpen })),
-  setCurrentCategoryIndex: (i) => set(() => ({ currentCategoryIndex: i })),
+
+  isMessageModalOpen: false,
+  messageModalContent: { title: "", message: "" },
+  setMessageModalContent: (content) =>
+    set(() => ({ messageModalContent: { ...content, loading: false } })),
+  setLoadingModalContent: async <T>(
+    promise: Promise<T>,
+    resolver: (input: T) => MessageModalContent
+  ) => {
+    set(({ messageModalContent }) => ({
+      messageModalContent: { ...messageModalContent, loading: true },
+    }));
+    const content = await promise.then(resolver);
+    set(() => ({ messageModalContent: { ...content, loading: false } }));
+  },
 }));
